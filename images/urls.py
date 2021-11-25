fbde5d184c246d13f0bf017888e0c59a20b9e0a5
wwlodar <58707277+wwlodar@users.noreply.github.com>
2021-11-25 14:57:31 +0100

    fixes

--- images/urls.py
+++ images/urls.py
@@ -6,4 +6,3 @@ urlpatterns = [
     path('list/', ImagesListView.as_view(), name='image-list'),
     path('api-login', LoginView.as_view(), name='api-login'),
 ]
-handler500 = 'images.views.this_server_error'
